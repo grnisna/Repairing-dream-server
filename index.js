25b8cbce452be079979f43f544a21d6dda59f79b
grnisna <mdnisanahmed63@gmail.com>
2023-02-13 02:40:47 +0600

    something added

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log() 
+console.log();
 
-app.get('/', (req,res)=>{
-    res.send('server is running')
-})
-
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 //user : allUser
 //pass : 8IFQMQJr1tFXWUdg
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1tgaxyp.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        app.get('/services', async (req,res)=>{
-            const serviceCollection = client.db('repairing_dream').collection('users');
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+async function run() {
+  try {
+    const serviceCollection = client.db("repairing_dream").collection("services");
+    const orderCollection = client.db("repairing_dream").collection("order");
 
-            res.send(services);
-            
-        })
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // find one service from database
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+    //   const query = {_id: new ObjectId(id)} (if has mongodb id)
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/order', async(req,res)=>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(order);
+    })
 
 
 
 
-    }
-    finally{}
+
+
+
+
+
+
+
+
+  } finally {
+  }
 }
 
-run().catch(error => console.log(error))
+run().catch((error) => console.log(error));
 
-
-
-
-app.listen(port, ()=>{
-    console.log(`port is running `, port);
-})
+app.listen(port, () => {
+  console.log(`port is running `, port);
+});
